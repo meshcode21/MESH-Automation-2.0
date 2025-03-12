7bb2299565fab69225af60247f5588682e8b59e4
mahesh <maheshudas2077@gmail.com>
2025-03-13 00:42:52 +0545

    a sequence to run the code in loop is done.

--- src/app.js
+++ src/app.js
@@ -5,42 +5,57 @@ import { CaptchaToText } from "./CaptchaToText.js";
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://dvprogram.state.gov/ESC/CheckStatus.aspx", {
-    waitUntil: "networkidle0",
-  });
-  await page.waitForSelector("#escbodytag");
+  while (true) {
+    await page.goto("https://dvprogram.state.gov/ESC/Default.aspx");
+    await page.waitForSelector("#escform");
 
-  //   initialization of input fields...
-  const CN_inputField = await page.waitForSelector("::-p-xpath(//*[@id='txtCN'])");
-  const LastName_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtLastName"])`);
-  const YOB_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtYOB"])`);
-  const Code_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtCodeInput"])`);
-  const SubmitButton = await page.waitForSelector(`::-p-xpath(//*[@id="btnCSubmit"])`);
+    await page.click(`::-p-xpath(//*[@id="main"]/div[2]/div/p[3]/a)`);
 
-  
-  //   initialization of client data...
-  const cn = "20259SC6X7G99LKE";
-  const lastName = "udas";
-  const yob = "2003";
-  
-  //   typing data in input fields...
-  await CN_inputField.type(cn);
-  await LastName_inputField.type(lastName);
-  await YOB_inputField.type(yob);
-  
-//   await new Promise((resolve) => setTimeout(resolve, 2000));
+    //   initialization of input fields...
+    const CN_inputField = await page.waitForSelector("::-p-xpath(//*[@id='txtCN'])");
+    const LastName_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtLastName"])`);
+    const YOB_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtYOB"])`);
 
-  //   save captcha image...
-  const CaptchaImageElement = await page.waitForSelector(`::-p-xpath(//*[@id="c_checkstatus_uccaptcha30_CaptchaImage"])`);
-  await CaptchaImageElement.screenshot({ path: "captcha.png" });
+    //   initialization of client data...
+    const cn = "20259SC6X7G99LKE";
+    const lastName = "udas";
+    const yob = "2003";
 
-  //convert and fill the captcha text...
-  const captchaText = await CaptchaToText();
-//   console.log(captchaText);
-  await Code_inputField.type(captchaText!=""?captchaText:"xxxx");
+    //   typing data in input fields...
+    await CN_inputField.type(cn);
+    await LastName_inputField.type(lastName);
+    await YOB_inputField.type(yob);
 
-//   await SubmitButton.click();
+    //   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+    for (let i = 0; i < 6; i++) {
+      const SubmitButton = await page.waitForSelector(`::-p-xpath(//*[@id="btnCSubmit"])`);
+
+      //   save captcha image...
+      const CaptchaImageElement = await page.waitForSelector(`::-p-xpath(//*[@id="c_checkstatus_uccaptcha30_CaptchaImage"])`);
+      await CaptchaImageElement.screenshot({ path: "captcha.png" });
+
+      //convert and fill the captcha text...
+      const Code_inputField = await page.waitForSelector(`::-p-xpath(//*[@id="txtCodeInput"])`);
+      const captchaText = await CaptchaToText();
+      await Code_inputField.type(captchaText != "" ? captchaText : "xxxx");
+
+      await SubmitButton.focus();
+      // await new Promise((resolve) => setTimeout(resolve, 100));
+      await SubmitButton.click();
+
+      // logic to check the captcha is correct or not...
+      const container = await page.waitForSelector(`::-p-xpath(//*[@id="main"])`);
+      const captchaRight = await container.evaluate((el) => {
+        if (el.childElementCount < 2) return true;
+        else return false;
+      });
+
+      if (captchaRight) break;
+    }
+  }
+
+  // page.click(`::-p-xpath(//*[@id="link"]/div/div/span/a[2])`);
   // wait for 3 sec...
   // browser.close();
 })();
